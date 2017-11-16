6cb0879cbd2e9990860664dbd36cb67373308653
nielsboeckx1 <33620064+nielsboeckx1@users.noreply.github.com>
2017-11-16 16:20:27 +0100

    Add files via upload

--- donnees.c
+++ donnees.c
@@ -2,38 +2,36 @@
 #include <stdlib.h>
 #include "donnees.h"
 
-int main(void)
+void lirefichier()
 {
 
     FILE *fic = fopen("test.csv","r"); // FILE est une structure qui contient le pointeur *fic qui pointe vers un flux  (le fichier)
 
-
-
 typedef struct poul_temps poul_temps;
-struct poul_temps;
+
+struct poul_temps
 {
     int temps;
-    int poul;
+    int poul; // on declare les deux tableaux d'entier présent dans la structure poul_temps
 
 };
  int i;
- int temps[20];
- int poul[20];    // on declare les deux entiers prÃ©sents dans le fichier csv
+poul_temps t[20];
 
 
- if(fic == NULL) // si le fichier n'est pas ouvert on quitte le programme
+ if(&fic == NULL) // si le fichier n'est pas ouvert on quitte le programme
     exit(1);
 
 for(i = 0; i < 20; i++)
     {
-   fscanf(fic, "%d ; %d\n", &poul_temps.temps[i], &poul_temps.poul[i]);
+fscanf(fic, "%d ; %d\n", &t[i].temps, &t[i].poul); // associer les valeurs du fichier aux entiers respectif dans le tableau t de structures poul_temps
 
-printf("poul : %d    ", poul[i]);  // test pour verifier que les variables ont bien pris la valeur de poul et de temps
-printf("temps : %d\n", temps[i]);
+printf("poul : %d    ", t[i].poul);  // test pour verifier que les variables ont bien pris la valeur de poul et de temps
+printf("temps : %d\n", t[i].temps);
 
 }
 
 
     fclose(fic);
-return 0;
+
 }
